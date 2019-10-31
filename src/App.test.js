a0d4f186bb9ca0677d13077b7a417348bd6d0321
Joe <joe.@Joes-MacBook.local>
2019-10-31 08:43:18 -0600

    second

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,24 @@
 import React from "react";
 import { shallow } from "enzyme";
-import App from "./App";
+import renderer from "react-test-renderer";
+
+import App, { Pagination } from "./App";
 
 it("renders without crashing", () => {
   shallow(<App />);
+});
+
+describe("App", () => {
+  test("snapshot renders", () => {
+    const component = renderer.create(<App />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+describe("Counter", () => {
+  test("snapshot renders", () => {
+    const component = renderer.create(<Pagination cursor={1} />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
